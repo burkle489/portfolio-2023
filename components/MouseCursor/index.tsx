1b783fe23988806fbb8d3a32f97bfde42a5e9079
taylerburkefluid <tayler.burke@fluidesign.co.uk>
2023-06-21 22:41:08 +0100

    fic

--- components/MouseCursor/index.tsx
+++ components/MouseCursor/index.tsx
@@ -34,21 +34,25 @@ export const MouseCursor = () => {
       timeline.current.reverse()
     }
   }, [isHovering])
-  return (
-    <div
-      className="fixed w-14 h-14 z-[9999] flex justify-center items-center mix-blend-exclusion pointer-events-none"
-      style={{ top: `${mouse.y - 28}px`, left: `${mouse.x - 28}px` }}
-    >
-      <div className="relative w-full h-full">
-        <div
-          ref={cursor}
-          className="w-12 h-12 bg-yellow rounded-full absolute top-1 left-1 scale-100"
-        ></div>
-        <div ref={hoverCursor} className="w-full h-full scale-0">
-          <div className="bg-yellow w-14 h-14 rounded-full abolute left-0 top-0 opacity-30"></div>
-          <div className="w-8 h-8 bg-yellow rounded-full absolute top-3 left-3"></div>
+  if (mouse.x && mouse.y) {
+    return (
+      <div
+        className="fixed w-14 h-14 z-[9999] flex justify-center items-center mix-blend-exclusion pointer-events-none"
+        style={{ top: `${mouse.y - 28}px`, left: `${mouse.x - 28}px` }}
+      >
+        <div className="relative w-full h-full">
+          <div
+            ref={cursor}
+            className="w-12 h-12 bg-yellow rounded-full absolute top-1 left-1 scale-100"
+          ></div>
+          <div ref={hoverCursor} className="w-full h-full scale-0">
+            <div className="bg-yellow w-14 h-14 rounded-full abolute left-0 top-0 opacity-30"></div>
+            <div className="w-8 h-8 bg-yellow rounded-full absolute top-3 left-3"></div>
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  } else {
+    return null
+  }
 }
